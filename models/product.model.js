f604d05cb74dcbc03f993de2e342018cd004df14
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 11:15:12 +0100

    refactor: Update product model schema and add timestamps

--- models/product.model.js
+++ models/product.model.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Product name is required"],
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  quantity: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.export = Product;
