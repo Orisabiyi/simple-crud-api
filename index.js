aabd3ad0ac72eaf3e034ffa55041fca8e0f1b283
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 12:57:35 +0100

    feat: Update API endpoint for retrieving products

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ app.get("/", function (req, res) {
   res.send("Hello from Node Server");
 });
 
-app.post("/api/product", async function (req, res) {
+app.get("/api/products", async function (req, res) {
+  try {
+    const product = await Product.find();
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async function (req, res) {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
