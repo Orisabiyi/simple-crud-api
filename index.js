6cd2884e5ea36c795b9aeab94bab85096f41a31a
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 07:45:06 +0100

    chore: Update .gitignore to include mongopwd.md and modify index.js to improve server response

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 app.listen(3000, () => console.log("Server is running at port 3000"));
 
 app.get("/", function (req, res) {
-  res.send("Hello from Node API Server");
+  res.send("Hello from Node Server");
 });
