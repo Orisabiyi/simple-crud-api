8f02335bc3a8418cf878271d6b53224c85f1df7f
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 07:33:08 +0100

    chore: Add nodemon as a dev dependency and update package.json

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => console.log("Server is running at port 3000"));
+
+app.get("/", function (req, res) {
+  res.send("Hello from Node API Server");
+});
