b4764145a3040aa1c7a2c2648141eb40d0d35fd2
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 08:16:32 +0100

    chore: Update package.json to include mongoose as a dependency

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,3 +8,10 @@ app.listen(3000, () => console.log("Server is running at port 3000"));
 app.get("/", function (req, res) {
   res.send("Hello from Node Server");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://orisabiyidavid:W1hKbcu0o44UOuag@simple-crud-api.bnjnd.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=simple-crud-api"
+  )
+  .then(() => console.log("Database connected"))
+  .catch(() => console.log("Error connecting to database"));
