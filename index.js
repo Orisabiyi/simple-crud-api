a8783c960f11947aeeb135057b0e31fc05319d27
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-13 11:04:37 +0100

    chore: Update index.js to include server startup after database connection

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running at port 3000"));
-
 app.get("/", function (req, res) {
   res.send("Hello from Node Server");
 });
@@ -13,5 +11,8 @@ mongoose
   .connect(
     "mongodb+srv://orisabiyidavid:W1hKbcu0o44UOuag@simple-crud-api.bnjnd.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=simple-crud-api"
   )
-  .then(() => console.log("Database connected"))
+  .then(() => {
+    console.log("Database connected");
+    app.listen(3000, () => console.log("Server is running at port 3000"));
+  })
   .catch(() => console.log("Error connecting to database"));
