839d23d53fc4c49655cdea17a4ba16b2ccbb9a45
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-15 12:34:15 +0100

    refactor: Separate createProduct API endpoint into its own function

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -19,3 +19,12 @@ const getProduct = async function (req, res) {
     res.status(500).json({ message: error.message });
   }
 };
+
+const createProduct = async function (req, res) {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
