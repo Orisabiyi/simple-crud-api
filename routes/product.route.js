f0c6280f6075ef704dd9d6e360f8f8d707128834
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-15 12:47:10 +0100

    refactor: Separate createProduct API endpoint into its own function

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+// get route for api endpoints
 router.get("/", getProducts);
-
 router.get("/:id", getProduct);
+
+router.post("/", createProduct);
