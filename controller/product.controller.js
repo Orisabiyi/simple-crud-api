e766856cc8ff3a038a4f781afcb6fc358c3ac0cd
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-15 12:38:34 +0100

    refactor: Separate updateProduct API endpoint into its own function

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -28,3 +28,26 @@ const createProduct = async function (req, res) {
     res.status(500).json({ message: error.message });
   }
 };
+
+const updateProduct = async function (req, res) {
+  try {
+    const { id } = req.params;
+
+    // Validating Id
+    if (!validateObjectId(id))
+      return res.send(400).json({ message: "Product Id is not valid" });
+
+    const findProduct = await Product.findById(id);
+
+    // validating product existence
+    if (!findProduct)
+      return res.status(404).json({ message: "Product does not exist" });
+
+    const updateProduct = await Product.findByIdAndUpdate(id, req.body);
+    const newProduct = await Product.findById(id);
+    res.status(200).send(newProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
