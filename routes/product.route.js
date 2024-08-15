e3c9d4815d5cd6d8bff30293c62aa573ee9fa43e
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-15 12:56:02 +0100

    refactor: Separate product.route.js into individual API endpoint functions

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/product.controller.js");
 
 // get route for api endpoints
 router.get("/", getProducts);
