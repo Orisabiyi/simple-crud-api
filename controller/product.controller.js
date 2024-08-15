a748f76d5a395d7833a13e0105beb30c9a07b79d
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-15 12:32:47 +0100

    feat: Add getProducts API endpoint

---
+++ controller/product.controller.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Product = require("../models/product.model.js");
+
+const getProducts = async function (req, res) {
+  try {
+    const product = await Product.find();
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
