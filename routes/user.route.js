a31b25584dd1f73c64e54c2214430f43bed45cfa
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-16 14:31:49 +0100

    refactor: Add createUser and loginUser API endpoints for user authentication and registration

---
+++ routes/user.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const { createUser, loginUser } = require("../controller/user.controller.js");
+
+router.post("/create", createUser);
+router.get("/login", loginUser);
