1f80782f05e3b259af29e3f50f97052e3fc5d7b1
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-16 13:32:42 +0100

    refactor: Add loginUser API endpoint for user authentication

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -21,6 +21,26 @@ const createUser = async function (req, res) {
   }
 };
 
+const loginUser = async function (req, res) {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    // Check if user exist or password is valid
+    if (!user || !(await bcrypt.compare(password, user.password)))
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createUser,
+  loginUser,
 };
