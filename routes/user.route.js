777574d687190a2e735e1ae7d63a09ffa5a3993c
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-17 13:18:31 +0100

    refactor: Add verify OTP route to user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUser } = require("../controller/user.controller.js");
+const {
+  createUser,
+  verifyUser,
+  loginUser,
+} = require("../controller/user.controller.js");
 
 router.post("/create", createUser);
+router.get("/verify-otp", verifyUser);
 router.get("/login", loginUser);
 
 module.exports = router;
