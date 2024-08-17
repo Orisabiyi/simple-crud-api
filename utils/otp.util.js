ef43c1a500c29340b0161fb5cad09274cf404ab8
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-17 07:36:40 +0100

    refactor: Add OTP email functionality for user authentication

---
+++ utils/otp.util.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const generateOTP = () => Math.floor(1000 + Math.random() * 9000);
+
+const sendOTP = async function (email, otp) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.COMPANY_EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.COMPANY_EMAIL,
+    to: email,
+    subject: "Your OTP Code",
+    text: `Your OTP code is ${otp}`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = {
+  generateOTP,
+  sendOTP,
+};
